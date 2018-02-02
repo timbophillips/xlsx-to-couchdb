6cc0e493da423c8b430b6665d4109456a0b91970
timbophillips <timothy.john.phillips@gmail.com>
2018-02-02 23:46:39 +0800

    Add files via upload

---
+++ xls-to-couchdb.ts
@@ -0,0 +1,67 @@
+import { readFile, utils, WorkBook } from "xlsx";
+import { argv } from "yargs";
+import { request } from 'http';
+import { render } from 'prettyjson';
+
+// make sure the user has supplied xlsx filename and couchdb
+if (argv._.length < 2) {
+  console.log('required argumnets: xls_filename couchdb_database_name');
+  process.exit();
+}
+const xlsxFile: string = argv._[0];
+const couchdbName: string = argv._[1];
+
+// tell user what we are using
+console.log(
+  "the excel file is %s and the couchdb name is %s",
+  xlsxFile,
+  couchdbName
+);
+
+// options set for the http post
+// couchdb on default port at localhost
+const postOptions = {
+  hostname: '127.0.0.1',
+  port: 5984,
+  path: '/' + couchdbName + '/_bulk_docs',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+};
+
+// use xlsx library to read the xlsx file to an object
+const workbook = readFile(xlsxFile);
+
+// loop through the worksheets in the xlsx workbook
+workbook.SheetNames.forEach(sheetName => {
+
+  // use xlsx library utils to convert worksheet to JSON
+  const jsonOutput = utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+  // tell the user whats happening
+  console.log('sheet %s in workbook %s converted to JSON:', sheetName, xlsxFile);
+  console.log(render(jsonOutput));
+  console.log('uploading %s as JSON to %s', sheetName, couchdbName);
+
+  // old school nodeJS http post (with prettyjson library)
+  const req = request(postOptions, res => {
+    console.log('Status: ' + res.statusCode);
+    console.log('Headers: ' + render(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', body => {
+      console.log('Body: ' + render(JSON.parse(body as string)));
+    });
+  });
+  req.on('error', e => {
+    console.log('problem with request: ' + e.message);
+  });
+  // write data to request body
+  // note the addition of docs: to the JSON string to 
+  // make it compatible with couchDB bulk_docs upload
+  req.write('{"docs": ' + JSON.stringify(jsonOutput) + '}');
+  req.end();
+
+});
+
+
